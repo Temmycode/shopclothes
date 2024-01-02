5b6a35724f9645b8099feb5a8e0791c4bf0c7b3c
Akisanya Temiloluwa <tolutech2004@gmail.com>
2024-01-02 17:33:22 +0100

    Initial Commit

---
+++ ShopClothes/ShopClothesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopClothesApp.swift
+//  ShopClothes
+//
+//  Created by Temiloluwa Akisanya on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopClothesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
